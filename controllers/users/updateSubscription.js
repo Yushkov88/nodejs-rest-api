881aab08d4d4807c5169cb97fa7974369f5dac1f
Yushkov Vitaliy <yushkov1988@gmail.com>
2023-01-18 13:46:59 +0200

    fix bugs

--- controllers/users/updateSubscription.js
+++ controllers/users/updateSubscription.js
@@ -1,15 +1,13 @@
 const { User } = require("../../models");
-const RequestError = require("../../helpers/RequestError");
 
 const updateSubscription = async (req, res) => {
-  if (Object.keys(req.body).length === 0) {
-    throw RequestError(400, "missing field subscription");
-  }
   const { _id } = req.user;
-  const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
-  if (!result) {
-    throw RequestError(404, "Not found");
-  }
+  const { subscription } = req.body;
+  const result = await User.findByIdAndUpdate(
+    _id,
+    { subscription },
+    { new: true }
+  );
   res.json(result);
 };
 
